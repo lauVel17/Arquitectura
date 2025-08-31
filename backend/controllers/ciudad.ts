6f30f1979eb0e7389eb03165d898c6da88ec5b68
xeleneaperez <xperez7@estudiantes.areandina.edu.co>
2025-08-31 16:43:41 -0500

    correción ciudad

--- backend/controllers/ciudad.ts
+++ backend/controllers/ciudad.ts
@@ -1,17 +1,39 @@
-import ciudad from "../models/ciudad";
+
 import { Request, Response } from "express";
+import ciudad from "../models/ciudad";
+
+//consultar todas las ciudades
+export const consultarciudad = async (req: Request, res: Response) => {
+  try {
+    const ciudades = await ciudad.findAll({
+      /* order: [["idciudad", "DESC"]], */
+    });
+
+    if (ciudades.length === 0) {
+      return res.status(400).json({ msg: "No se encontraron las  ciudades" });
+    }
+
+    res.json(ciudades);
+  } catch (error) {
+    console.error("Error al consultar las ciudades:", error);
+    res.status(500).json({
+      msg: "No se pudo completar la operación. Intenta nuevamente más tarde o contacte al administrador.",
+    });
+  }
+};
 //Crear ciudad
-export const createCity = async(req:Request, resp:Response)=>{
-    const{body}=req;
-  try{
-    console.log("Datos recibidos ",body);
-    const ciudadC =await ciudad.create({
-        idciudad: body.idciudad,
-        iddepartamento: body.iddepartamento,
-        nombre: body.nombre,
+export const createCiudad = async (req: Request, resp: Response) => {
+  const { body } = req;
+  try {
+    console.log("Datos recibidos ", body);
+    const ciudadC = await ciudad.create({
+     idciudad: body.idciudad,
+     iddepartamento: body.iddepartamento,
+     nombre: body.nombre,
     });
     resp.status(200).json({
-        msg:"El usuario ha sido creado exitosamente,ciudad",ciudadC,
+      msg: "La ciudad ha sido creado exitosamente",
+      ciudadC,
     });
     }catch(error){
         console.log("Error al crear la ciudad",error);
@@ -19,63 +41,63 @@ export const createCity = async(req:Request, resp:Response)=>{
             msg:"No se logró completar la creación de la ciudad",
         });
     }
-    
-};
+ }
 
 //Consultar ciudad
-export const  consulCity= async( req:Request, res: Response)=>{
-        const {id} = req.params;
-        const ciudadc= await ciudad.findByPk(id);
-            if (!ciudadc){
-                res.json(ciudadc); 
-            }else{
-                res.status(400).json({
-                msg:"No se encontro la ciudad especificada",
-                });     
-      }
-     };
-      
-//Actualizar ciudad
-export const updateCity =async (req: Request,res: Response)=>{
-const{id} =req.params;
-const{body}=req;
-try{
-    const updatecity=await ciudad.findByPk(id);
-    if(!updatecity){
-      return
-        res.status(404).json({
-        msg:"No se encontro la ciudad ",
-        });
+export const consultCiudad = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const ciudadCons = await ciudad.findByPk(id);
+  if (!ciudadCons) {
+    res.json(ciudadCons);
+  } else {
+    res.status(400).json({
+      msg: "No se encontro ciudad especificada",
+    });
+  }
+};
+
+//Actualizar Ciudad
+export const updateCiudad = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { body } = req;
+  try {
+    const actC= await ciudad.findByPk(id);
+    if (!actC) {
+      return res.status(404).json({
+        msg: "No se encontró la ciudad",
+      });
     }
-    console.log("Datos recibidos para proceder la actualización",body);
-    await updatecity.update({
-        
+
+    console.log("Datos recibidos para proceder la actualización", body);
+    await actC.update({
+      //idciudad: body.idciudad,
+      iddepartamento: body.iddepartamento,
+      nombre:body.nombre,
     });
     res.status(200).json({
-        msg:"La ciudad se ha actualizado correctamente",updatecity,
+      msg: "La ciudad se ha actualizado correctamente",
+      actC,
     });
-} catch(error){
+  } catch (error) {
     console.log(error);
     res.status(500).json({
-        msg:"No se pudo completar la actualización de la ciudad, intente nuevamente más tarde.",
+      msg: "No se pudo completar la actualización de la ciudad, intente nuevamente más tarde.",
     });
-}
+  }
 };
-
 
 //Eliminar ciudad
-export const delateCity =async(req: Request, res: Response)=>{
-const{id}=req.params;
-const deletecit= await ciudad.findByPk(id);
-if(!ciudad){
-    return
-res.status(404).json({
-    msg:"No se encontro el usuario"
-});
-}
- await ciudad.destroy();
- res.status(200).json({
-    msg:"La ciudad ha sido eliminada de forma correcta",ciudad,
- });
+export const delateCiudad = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const deleteciud = await ciudad.findByPk(id);
+  if (!deleteciud) {
+    return res.status(404).json({
+      msg: "No se encontro la ciudad",
+    });
+  }
+  await deleteciud.destroy();
+  res.status(200).json({
+    msg: "La ciudad ha sido eliminada de forma correcta",
+    ciudad,
+  });
 };
-   
