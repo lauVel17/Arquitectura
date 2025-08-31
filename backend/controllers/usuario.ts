a11b988eb3edfb38bd42bb1f913bdea55aa76371
xeleneaperez <xperez7@estudiantes.areandina.edu.co>
2025-08-31 13:23:12 -0500

    controlador usuario

---
+++ backend/controllers/usuario.ts
@@ -0,0 +1,91 @@
+import usuario from "../models/usuarios";
+import { Request, Response } from "express";
+//Crear usuario
+export const createUser = async(req:Request, resp:Response)=>{
+    const{body}=req;
+  try{
+    console.log("Datos recibidos ",body);
+    const usuarioc =await usuario.create({
+        nodocumento: body.nodocumento,
+        nombreapellido: body.nombreapellido,
+        correo: body.correo,
+        telefono: body.telefono,
+        estado: body.estado,
+        fechaingreso:body.fechaingreso,
+        ciudadid: body.ciudad,
+    });
+    resp.status(200).json({
+        msg:"El usuario ha sido creado exitosamente",usuarioc,
+    });
+    }catch(error){
+        console.log("Error al crear el usuario",error);
+        resp.status(500).json({
+            msg:"No se logró completar la cración del usuario",
+        });
+    }
+    
+};
+
+//Consultar usuario
+export const  consultUserid= async( req:Request, res: Response)=>{
+        const {id} = req.params;
+        const usuarioc= await usuario.findByPk(id);
+            if (!usuarioc){
+                res.json(usuarioc); 
+            }else{
+                res.status(400).json({
+                msg:"No se encontro usuario especificado",
+                });     
+      }
+     };
+      
+//Actualizar usuario
+export const updateUser =async (req: Request,res: Response)=>{
+const{id} =req.params;
+const{body}=req;
+try{
+    const actus=await usuario.findByPk(id);
+    if(!actus){
+      return
+        res.status(404).json({
+        msg:"No se encontro el usuario ",
+        });
+    }
+    console.log("Datos recibidos para proceder la actualización",body);
+    await actus.update({
+        // nodocumento: body.nodocumento,
+        nombreapellido: body.nombreapellido,
+        correo: body.correo,
+        telefono: body.telefono,
+        estado: body.estado,
+        fechaingreso:body.fechaingreso,
+        ciudadid: body.ciudad,
+    });
+    res.status(200).json({
+        msg:"El usuario se ha actualizado correctamente",actus,
+    });
+} catch(error){
+    console.log(error);
+    res.status(500).json({
+        msg:"No se pudo completar la actualización del usuario, intente nuevamente más tarde.",
+    });
+}
+};
+
+
+//Eliminar usuario
+export const delateusua =async(req: Request, res: Response)=>{
+const{id}=req.params;
+const deleteus= await usuario.findByPk(id);
+if(!usuario){
+    return
+res.status(404).json({
+    msg:"No se encontro el usuario"
+});
+}
+ await usuario.destroy();
+ res.status(200).json({
+    msg:"La ciudad ha sido eliminada de forma correcta",usuario,
+ });
+};
+   
